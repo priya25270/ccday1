5fbe50d0e98a7038a9c6c72769945b91edf2ae06
priya25270 <km894274@gmail.com>
2023-03-31 17:20:55 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Day2/day2';
+import Product from './Day2/daay2';
+import Footer from './Day2/daaay2';
+import Content from './Day2/content';
+
+function App() {
+  return (
+    <div >
+ <Header/>
+ <Product/>
+ <Content/>
+ <Footer/>
+    </div>
+  );
+}
+
+export default App;
